155e2e64bee8e1c7d3e5a27cc2cb7e796f02a617
dangillman <dangillman93@gmail.com>
2024-04-23 15:58:17 -0400

    added content, executed deliverables.

--- src/index.js
+++ src/index.js
@@ -1,3 +1,41 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+  const taskForm = document.getElementById("create-task-form");
+  taskForm.addEventListener("submit", handleFormSubmit);
+
+  const tasksList = document.getElementById("tasks");
+  tasksList.addEventListener("click", handleTaskDelete);
 });
+
+function handleFormSubmit(event) {
+  event.preventDefault();
+  const taskInput = document.getElementById("new-task-description");
+  const taskDescription = taskInput.value;
+
+  const newTask = createTaskElement(taskDescription);
+  appendTask(newTask);
+
+  taskInput.value = "";
+}
+
+function createTaskElement(taskDescription) {
+  const newTask = document.createElement("li");
+  newTask.innerText = taskDescription;
+
+  const deleteButton = document.createElement("button");
+  deleteButton.innerText = "Delete";
+  newTask.appendChild(deleteButton);
+
+  return newTask;
+}
+
+function appendTask(taskElement) {
+  const tasksList = document.getElementById("tasks");
+  tasksList.appendChild(taskElement);
+}
+
+function handleTaskDelete(event) {
+  if (event.target.tagName === "BUTTON") {
+    const taskItem = event.target.parentElement;
+    taskItem.remove();
+  }
+}
